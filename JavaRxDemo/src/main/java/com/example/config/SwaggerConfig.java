8d53da10591461857d36e324c75968fde158a7b9
Ranwala <URANWRA@PEROOT.COM>
2017-12-06 14:24:00 +0530

    code and config props were refactored.

--- JavaRxDemo/src/main/java/com/example/config/SwaggerConfig.java
+++ JavaRxDemo/src/main/java/com/example/config/SwaggerConfig.java
@@ -14,33 +14,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class ApplicationConfig {
-	/** The title for the spring boot service to be displayed on swagger UI. */
+public class SwaggerConfig {
 	@Value("${swagger.title}")
 	private String title;
 
-	/** The description of the spring boot service. */
 	@Value("${swagger.description}")
 	private String description;
 
-	/** The version of the service. */
 	@Value("${swagger.version}")
 	private String version;
 
-	/**
-	 * This method will return the Docket API object to swagger which will in
-	 * turn display the information on the swagger UI.
-	 * 
-	 * Refer the URL http://{ip-address or
-	 * host-name}:{service.port}/{server.contextPath}/swagger-ui.html
-	 * 
-	 * service.port and server.contextPath are provided in
-	 * application.properties. If these properties are not defined in the
-	 * application.properties then the URL for swagger will be
-	 * http://{ip-address or host-name}:8080/swagger-ui.html
-	 * 
-	 * @return the docket
-	 */
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
